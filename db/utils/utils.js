33e37e2259a0324db29f4adfe3a3c2fdfc79684e
Dominic Hui <dominichui112@gmail.com>
2020-01-20 15:09:09 +0000

    created util function formatDate in util.js file, to format SQL date to JS date object, function has been tested with TDD in seperate utils.spec.js, single item multi item and check for mutation.

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,26 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  // check length of list
+  if (list.length === 0) {
+    return [];
+  } else {
+    // duplicate list avoid mutation, extra precaution
+    const dataCopy = [...list];
+
+    // map thorugh list, modify created_at, return new array of objects
+    const formattedList = dataCopy.map(item => {
+      const formattedDate = new Date(item.created_at);
+
+      const newObj = {
+        ...item,
+        created_at: formattedDate
+      };
+
+      return newObj;
+    });
+
+    return formattedList;
+  }
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,79 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates take an array (list) of objects and return a new array. Each item in the new array must have its timestamp converted into a Javascript date object.", () => {
+  it("Single object within the array", () => {
+    const testData = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
 
-describe('makeRefObj', () => {});
+    const testOutput = formatDates(testData);
+    console.log(testOutput, " test");
 
-describe('formatComments', () => {});
+    expect(testOutput[0].created_at).to.be.a("date");
+  });
+  it("Multiple objects within the array", () => {
+    const testData = [
+      {
+        title: "Am I a cat?",
+        topic: "mitch",
+        author: "icellusedkars",
+        body:
+          "Having run out of ideas for articles, I am staring at the wall blankly, like a cat. Does this make me a cat?",
+        created_at: 280844514171
+      },
+      {
+        title: "Moustache",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "Have you seen the size of that thing?",
+        created_at: 154700514171
+      }
+    ];
+
+    const testOutput = formatDates(testData);
+
+    expect(testOutput[0].created_at).to.be.a("date");
+    expect(testOutput[1].created_at).to.be.a("date");
+  });
+  it("check original input has not been mutated", () => {
+    const testData = [
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ];
+
+    formatDates(testData);
+
+    expect(testData).to.eql([
+      {
+        title: "Living in the shadow of a great man",
+        topic: "mitch",
+        author: "butter_bridge",
+        body: "I find this existence challenging",
+        created_at: 1542284514171,
+        votes: 100
+      }
+    ]);
+  });
+});
+
+describe("makeRefObj", () => {});
+
+describe("formatComments", () => {});
