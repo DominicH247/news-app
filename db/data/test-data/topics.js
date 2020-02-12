5a7ecdc8154210f980dd21e110f62a414ee4c29f
Dominic Hui <dominichui112@gmail.com>
2020-02-12 21:46:03 +0000

    added topic_icon to test data and prod data

--- db/data/test-data/topics.js
+++ db/data/test-data/topics.js
@@ -1,14 +1,17 @@
 module.exports = [
   {
     description: "The man, the Mitch, the legend",
-    slug: "mitch"
+    slug: "mitch",
+    topic_icon: "https://image.flaticon.com/icons/svg/2302/2302836.svg"
   },
   {
     description: "Not dogs",
-    slug: "cats"
+    slug: "cats",
+    topic_icon: "https://image.flaticon.com/icons/svg/2302/2302836.svg"
   },
   {
     description: "what books are made of",
-    slug: "paper"
+    slug: "paper",
+    topic_icon: "https://image.flaticon.com/icons/svg/2302/2302836.svg"
   }
 ];
